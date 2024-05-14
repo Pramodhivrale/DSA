04644764d04230c899c95e81345350c8fd6ad9fb
Admin <hivralepramod4@gmail.com>
2024-05-14 18:58:00 +0530

    first commit

---
+++ src/com/pattern/FactorialOfNumber.java
@@ -0,0 +1,21 @@
+package com.pattern;
+
+import java.util.Scanner;
+
+public class FactorialOfNumber 
+{
+	public static void main(String[] args) {
+		 Scanner sc=new Scanner(System.in);
+		 System.out.println("Enter a number :");
+		 int fact=1;
+		 int num=sc.nextInt();
+		 
+		 for(int i=1;i<=num;i++)
+		 {
+             fact=fact * i;
+		 }
+		 System.out.println(fact);
+		 
+	}
+
+}
